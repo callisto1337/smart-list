32deab563bee921207914776c52f2e8e2e50ea1f
callisto1337 <hazardous333@gmail.com>
2018-04-02 22:32:29 +0300

    добавлен выбор заметок

--- public/js/index.js
+++ public/js/index.js
@@ -12,24 +12,37 @@ const app = new Vue({
     el: `#app`,
     data: {
         new_task: ``,
-        list: storage.getData()
+        list: storage.getData(),
+        listSelected: []
     },
     methods: {
         pushTask: function() {
-            if (this.new_task) {
-                this.list.push({
-                    id: this.list.length,
-                    text: this.new_task
-                });
-                this.new_task = ``;
-                storage.saveData(this.list);
+            if (!this.new_task || this.list.indexOf(this.new_task) > -1) {
+                return;
             }
+
+            this.list.push(this.new_task);
+            this.new_task = ``;
+            storage.saveData(this.list);
         },
-        deleteTask: function(id) {
-            if (confirm('Вы уверен, что хотите удалить задачу?')) {
-                Vue.delete(this.list, id);
-                storage.saveData(this.list);
+        selectTask: function(item) {
+            const selected = this.listSelected;
+
+            if (selected.indexOf(item) === -1) {
+                selected.push(item);
+                return;
             }
+            selected.splice(selected.indexOf(item), 1);
+        },
+        deleteTasks: function() {
+            this.list = this.list.filter(function(item) {
+                if (this.listSelected.indexOf(item) === -1) {
+                    return item;
+                }
+            }, this);
+
+            storage.saveData(this.list);
+            this.listSelected = [];
         }
     }
-})
+});
