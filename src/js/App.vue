abc5e6b9e2cad43a9fc2cf9a85890035e8fd9632
callisto1337 <hazardous333@gmail.com>
2018-05-08 14:00:06 +0300

    добавлен шаблон для авторизации

--- src/js/App.vue
+++ src/js/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <Header/>
-    <h1 class="text-center mt-3">Smarty</h1>
     <router-view/>
   </div>
 </template>
--- src/js/router/index.js
+++ src/js/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import List from '../components/List';
+import Auth from '../components/Auth';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
 			path: '/',
 			name: 'List',
 			component: List,
-		}
+		},
+		{
+			path: '/auth',
+			name: 'Auth',
+			component: Auth,
+		},
 	]
 });
