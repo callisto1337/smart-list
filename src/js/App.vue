1a24c159ba4d0e63132dbd9b0136e8c517de020e
callisto1337 <hazardous333@gmail.com>
2018-06-21 15:07:30 +0300

    Подключил код для базы данных

--- src/js/App.vue
+++ src/js/App.vue
@@ -1,55 +1,56 @@
 <template>
-  <div v-if="loading" class="text-center mt-5 pt-5">
-    <h3>Loading...</h3>
-  </div>
-  <div v-else>
-    <Header :authStatus="authStatus" />
-    <MainPage :authStatus="authStatus" />
-    <Footer/>
-  </div>
+    <div v-if="loading" class="text-center mt-5 pt-5">
+        <h3>Loading...</h3>
+    </div>
+    <div v-else>
+        <Header :authStatus="authStatus"/>
+        <MainPage :authStatus="authStatus"/>
+        <Footer/>
+    </div>
 </template>
 
 <script>
-  import MainPage from './pages/MainPage';
-	import Header from './components/Header';
-	import Footer from './components/Footer';
-	import firebase from 'firebase/app';
-	import config from './config/config';
-	import * as firebaseui from 'firebaseui';
+    import MainPage from './pages/MainPage';
+    import Header from './components/Header';
+    import Footer from './components/Footer';
+    import firebase from 'firebase/app';
+    import 'firebase/database';
+    import config from './config/config';
+    import * as firebaseui from 'firebaseui';
 
-	export default {
-		components: {
-			MainPage,
-			Header,
-			Footer
-		},
-    data() {
-			return {
-				userData: null,
-        authStatus: false,
-        loading: true
-      }
-    },
-		beforeCreate() {
-			firebase.initializeApp(config);
+    export default {
+        components: {
+            MainPage,
+            Header,
+            Footer
+        },
+        data() {
+            return {
+                userData: null,
+                authStatus: false,
+                loading: true
+            }
+        },
+        beforeCreate() {
+            firebase.initializeApp(config);
 
-			firebase.auth().onAuthStateChanged((user) => {
-				if (user) {
-					this.userData = user;
-					this.authStatus = true;
+            firebase.auth().onAuthStateChanged((user) => {
+                if (user) {
+                    this.userData = user;
+                    this.authStatus = true;
+                }
+                this.loading = false;
+            })
+        },
+        methods: {
+            logIn() {
+                const provider = new firebase.auth.GoogleAuthProvider();
+                firebase.auth().signInWithRedirect(provider);
+            },
+            logOut() {
+                firebase.auth().signOut();
+                window.location.reload();
+            }
         }
-				this.loading = false;
-			})
-		},
-    methods: {
-			logIn() {
-				const provider = new firebase.auth.GoogleAuthProvider();
-				firebase.auth().signInWithRedirect(provider);
-			},
-			logOut() {
-				firebase.auth().signOut();
-				window.location.reload();
-			}
     }
-	}
 </script>
--- src/js/components/List.vue
+++ src/js/components/List.vue
@@ -1,85 +1,92 @@
 <template>
-  <div class="col-lg-6 col-md-8 m-auto">
-    <h1 class="text-center mt-5">Smarty</h1>
-    <form v-on:submit.prevent="pushTask" class="input-group mt-3">
-      <input v-model.trim="newTask" class="form-control" type="text" maxlength="30" id="input" autocomplete="off">
-      <div class="input-group-append">
-        <input class="btn btn-outline-info" type="submit" value="+">
-      </div>
-    </form>
-    <p class="mt-2 m-0">
-      Активных задач:
-      <span class="badge badge-info" v-cloak>{{ tasks.length }}</span>
-      <span v-if="selectedTasks.length > 0" class="badge badge-primary" v-cloak>{{ selectedTasks.length }}</span>
-      <span
-          v-if="selectedTasks.length > 0"
-          v-on:click="deleteTasks()"
-          class="btn btn-danger float-right btn-sm"
-          v-cloak>×</span>
-    </p>
-    <div class="mt-3 form-row">
-      <span
-        v-bind:class="selectedTasks.indexOf(item) > -1 ? 'badge-primary' : 'badge-info'"
-        v-for="(item, key) in tasks" v-on:click="selectTask(item)"
-        :key="key"
-        class="badge badge-pill m-1 btn" v-cloak>
-        {{ item }}</span>
-      <span class="badge badge-pill m-1 btn badge-success" v-cloak>{{ newTask }}</span>
+    <div class="col-lg-6 col-md-8 m-auto">
+        <h1 class="text-center mt-5">Smarty</h1>
+        <form v-on:submit.prevent="pushTask" class="input-group mt-3">
+            <input v-model.trim="newTask" class="form-control" type="text" maxlength="30" id="input" autocomplete="off">
+            <div class="input-group-append">
+                <input class="btn btn-outline-info" type="submit" value="+">
+            </div>
+        </form>
+        <p class="mt-2 m-0">
+            Активных задач:
+            <span class="badge badge-info" v-cloak>{{ tasks.length }}</span>
+            <span v-if="selectedTasks.length > 0" class="badge badge-primary" v-cloak>{{ selectedTasks.length }}</span>
+            <span
+                v-if="selectedTasks.length > 0"
+                v-on:click="deleteTasks()"
+                class="btn btn-danger float-right btn-sm"
+                v-cloak>×</span>
+        </p>
+        <div class="mt-3 form-row">
+            <span
+                v-bind:class="selectedTasks.indexOf(item) > -1 ? 'badge-primary' : 'badge-info'"
+                v-for="(item, key) in tasks" v-on:click="selectTask(item)"
+                :key="key"
+                class="badge badge-pill m-1 btn" v-cloak>{{ item }}</span>
+            <span class="badge badge-pill m-1 btn badge-success" v-cloak>{{ newTask }}</span>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-	export default {
-		data: function () {
-			return {
-				newTask: ``,
-				tasks: this.getTasks(),
-				selectedTasks: []
-			}
-		},
-		watch: {
-			tasks: function () {
-				this.setTasks(this.tasks);
-			}
-		},
-		methods: {
-			pushTask: function () {
-				if (!this.newTask || this.tasks.indexOf(this.newTask) > -1) {
-					return;
-				}
+    import App from '../App';
+    import config from '../config/config';
+    import firebase from 'firebase/app';
+    import * as firebaseui from 'firebaseui';
 
-				this.tasks.push(this.newTask);
-				this.newTask = ``;
-			},
-			selectTask: function (item) {
-				const selected = this.selectedTasks;
+    export default {
+        data: function () {
+            return {
+                newTask: ``,
+                tasks: this.getTasks(),
+                selectedTasks: []
+            }
+        },
+        watch: {
+            tasks: function () {
+                this.setTasks(this.tasks);
+            }
+        },
+        methods: {
+            pushTask: function () {
+                if (!this.newTask || this.tasks.indexOf(this.newTask) > -1) {
+                    return;
+                }
 
-				if (selected.indexOf(item) === -1) {
-					selected.push(item);
-					return;
-				}
-				selected.splice(selected.indexOf(item), 1);
-			},
-			deleteTasks: function () {
-				this.tasks = this.tasks.filter((item) => {
-					return this.selectedTasks.indexOf(item) === -1
-				});
+                this.tasks.push(this.newTask);
+                this.newTask = ``;
+            },
+            selectTask: function (item) {
+                const selected = this.selectedTasks;
 
-				this.selectedTasks = [];
-			},
-			getTasks: function () {
-				return JSON.parse(localStorage.getItem(`data`)) || [];
-			},
-			setTasks: function (items) {
-				localStorage.setItem(`data`, JSON.stringify(items));
-			}
+                if (selected.indexOf(item) === -1) {
+                    selected.push(item);
+                    return;
+                }
+                selected.splice(selected.indexOf(item), 1);
+            },
+            deleteTasks: function () {
+                this.tasks = this.tasks.filter((item) => {
+                    return this.selectedTasks.indexOf(item) === -1
+                });
+
+                this.selectedTasks = [];
+            },
+            getTasks: function () {
+                const database = firebase.database();
+                console.log(database);
+
+                return JSON.parse(localStorage.getItem(`data`)) || [];
+            },
+            setTasks: function (items) {
+                localStorage.setItem(`data`, JSON.stringify(items));
+            }
+        }
     }
-  }
 </script>
 
 <style>
-  [v-cloak] {
-    display: none;
-  }
+    [v-cloak] {
+        display: none;
+    }
 </style>
